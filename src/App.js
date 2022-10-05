79e224ef56c516c3a72f65a41f2f44e3e2c97e88
Dumebi Okolo <okolodumebi@gmail.com>
2022-10-05 14:34:22 +0100

    new release

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import styles from './style'
+import Navbar from './components'
+import CarDeal from './components' 
+import Billing from './components'
+import Business from './components' 
+import Clients from './components'
+import  CTA from './components'
+import Stats from './components' 
+import Footer from './components'
+// import Testimonials from './components'
+import Hero from './components';
 
 
 function App() {
@@ -6,26 +16,26 @@ function App() {
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CarDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+        <Billing />
+        <CarDeal />
+        <Business />
+        <Clients />
+        <CTA />
+        <Stats />
+        <Footer />
+     
         </div>
       </div>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+
+import {close, logo, menu} from '../assest'
+import { navLinks } from '../constants'
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
